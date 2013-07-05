e2a4eab630e26d5bd81cbf5f9ab0526ea41abfca
Jun Takaki <dyundyun2910@gmail.com>
2013-07-05 21:56:38 +0900

    sample of coding Kata

---
+++ Kata/Bowling000/TestGame.cpp
@@ -0,0 +1,21 @@
+#include "stdafx.h"
+#include <vector>
+
+using namespace System;
+using namespace System::Text;
+using namespace System::Collections::Generic;
+using namespace	Microsoft::VisualStudio::TestTools::UnitTesting;
+
+namespace Bowling000
+{
+	[TestClass]
+	public ref class TestGame
+	{
+	public: 
+		[TestMethod]
+		void TestCreate()
+		{
+			
+		}
+	};
+}
